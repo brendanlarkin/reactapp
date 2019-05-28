20b13d094d0a608796c40ae224e32880f59df72e
brendanlarkin <49346107+brendanlarkin@users.noreply.github.com>
2019-05-28 15:29:16 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './DMTT.jpg';
+import './App.css';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Brendan Patrick Larkin
+        </p>
+        <a
+          className="App-link"
+          href="https://www.cosmic-symbolism.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          cool thing.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+export default App;
+
